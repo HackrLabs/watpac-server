c9b98bbfec1c8f7b33e7f0e14204f550663ba406
Danny Grove <danny@drgrovellc.com>
2015-03-13 21:58:15 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+var express = require('express'),
+    npm = require('npm'),
+    bower = require('bower'),
+    apiRouter = express.Router(),
+    app = express()
+
+
+apiRouter.get('/npm', function(req, res){
+    var q = req.query.q
+    npm.commands.search(q, true, 1, function(data){
+        res.send(data)
+    })
+})
+
+app.use('/api', apiRouter)
+
+npm.load({}, function(){
+    app.listen(8889, '0.0.0.0', function(){
+        console.log('Listening on http://%s:%s', '0.0.0.0', 8889)
+    })
+})
