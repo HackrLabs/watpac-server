483ae4cf380d1969f065a6b06ee802fd5f3acd60
Danny Grove <danny@drgrovellc.com>
2015-03-13 23:23:22 -0700

    Adds bower and ruby gems

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express'),
     npm = require('npm'),
     bower = require('bower'),
     apiRouter = express.Router(),
+    request = require('request'),
     app = express()
 
 
@@ -10,7 +11,35 @@ apiRouter.get('/npm', function(req, res){
     var q = req.query.q.split(',')
     console.log('Search Query: %s', q)
     npm.commands.search(q, false, function(err, data){
+        if(err) {
+            console.error('ERROR: ', err)
+            res.send(err) 
+        } else {
+            res.send(data)
+        }
+    })
+})
+
+apiRouter.get('/bower', function(req, res){
+    var q = req.query.q
+    bower.commands
+    .search(q, {})
+    .on('end', function(data){
         res.send(data)
+    })
+    .on('error', function(err){
+        res.send(err)
+    })
+})
+
+apiRouter.get('/gems', function(req, res){
+    var q = req.query.q
+    request('http://rubygems.org/api/v1/gems/' + q + '.json', function(err, response, body){
+        if(err) {
+            res.send(err)
+        } else {
+            res.send(body)
+        }
     })
 })
 
